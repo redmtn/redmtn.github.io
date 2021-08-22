03f2ae84e4b8ef5a3d5fb2e078acd462ffa8cba4
dfsek <dfsek@protonmail.com>
2021-08-22 13:43:45 -0700

    save schedule selection

--- resources/script/setup.js
+++ resources/script/setup.js
@@ -24,16 +24,28 @@ yamlResource("schools") // load schedules
                         let schoolSelect = $("#school_select");
                         schoolSelect.append(`<option value="${school}">${value[school].name}</option>`)
                         schoolSelect.on("change", () => updateScheduleSelection())
-                        updateScheduleSelection();
 
-                        $("#schedule_select").on("change", () => updateSelected());
+                        let scheduleSelect = $("#schedule_select");
+
+                        updateScheduleSelection(true);
+
+                        if (localStorage.getItem("school")) {
+                            schoolSelect.val(localStorage.getItem("school"))
+                        }
+
+                        if (localStorage.getItem("schedule")) {
+                            console.log(localStorage.getItem("schedule"))
+                            scheduleSelect.val(localStorage.getItem("schedule"))
+                        }
+
+                        scheduleSelect.on("change", () => updateSelected());
                     }
                 })
             });
         }
     );
 
-function updateScheduleSelection() {
+function updateScheduleSelection(init) {
     let select = $("#schedule_select");
     select.html(""); // Clear contents
 
@@ -44,11 +56,17 @@ function updateScheduleSelection() {
         select.append(`<option value="${schedule}">${schedules[schedule].name}</option>`)
     }
 
-    updateSelected();
+    updateSelected(init);
 }
 
-function updateSelected() {
-    schedule = scheduleData[$("#school_select").val()].schedules[$("#schedule_select").val()];
+function updateSelected(init) {
+    let school = $("#school_select").val();
+    let scheduleSelect = $("#schedule_select").val()
+    schedule = scheduleData[school].schedules[scheduleSelect];
+    if (!init) {
+        window.localStorage.setItem("school", school)
+        window.localStorage.setItem("schedule", scheduleSelect)
+    }
 }
 
 TaskQueue.add(() => {
