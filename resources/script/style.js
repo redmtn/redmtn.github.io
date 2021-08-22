f2e4256705f7b476d1452d774af055be92e16ec8
dfsek <dfsek@protonmail.com>
2021-08-22 02:02:59 -0700

    fix style overwrite

--- resources/script/style.js
+++ resources/script/style.js
@@ -8,7 +8,7 @@ var iFramed = (window !== window.parent)
 TaskQueue.add(() => {
     if (css && !iFramed) {
         if (window.localStorage.getItem("css_overwrite")) {
-            $("main_style").remove();
+            $("#main_style").remove();
             console.log("Overwriting CSS...")
         }
         $('head').append(`<style id="custom_css">${window.localStorage.getItem("css")}</style>`);
