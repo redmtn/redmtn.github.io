054c42b88ffa295cbf4f2e40ae11e1e591ff39c3
dfsek <dfsek@protonmail.com>
2021-08-22 01:59:20 -0700

    custom stylesheet impl

--- resources/script/style.js
+++ resources/script/style.js
@@ -5,6 +5,12 @@ console.log(css);
 
 var iFramed = (window !== window.parent)
 
-if(css && !iFramed) {
-    $('head').append(`<style id="custom_css"></style>`);
-}
+TaskQueue.add(() => {
+    if (css && !iFramed) {
+        if (window.localStorage.getItem("css_overwrite")) {
+            $("main_style").remove();
+            console.log("Overwriting CSS...")
+        }
+        $('head').append(`<style id="custom_css">${window.localStorage.getItem("css")}</style>`);
+    }
+})
--- style/stylesheets.js
+++ style/stylesheets.js
@@ -13,12 +13,30 @@ fetch("./stylesheets.yml")
 <p>${sheet.description}</p>
 <br>
 <iframe id="cont_${sheet.id}" src='../index.html'></iframe>
+<br>
+<input type="button" id="install_${sheet.id}" value="Install Stylesheet">
 </div>
 `)
                 let iframe = $(`#cont_${sheet.id}`);
                 iframe.on("load", () => {
                     iframe.contents().find("#main_style").attr("href", sheet.url.replace("{THIS}", window.location.origin))
-                })
+                });
+
+                let button = $(`#install_${sheet.id}`);
+
+                button.on("click", () => {
+                    console.log("Installing...")
+                    if(window.localStorage.getItem("css") && !confirm("Overwrite existing stylesheet?")) {
+                        return;
+                    }
+                    fetch(sheet.url.replace("{THIS}", window.location.origin))
+                        .then(css => css.text())
+                        .then(css => {
+                            window.localStorage.setItem("css", css);
+                            window.localStorage.setItem("css_overwrite", sheet.replace)
+                            window.localStorage.setItem("use_hms", sheet["short-times"])
+                        })
+                });
             })
         })
     })
