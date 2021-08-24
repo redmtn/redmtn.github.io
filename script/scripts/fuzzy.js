e42d77f9875ad4866159adaa93269d8acd7f0c90
dfsek <dfsek@protonmail.com>
2021-08-23 21:04:39 -0700

    funnier fuzzy clock

--- script/scripts/fuzzy.js
+++ script/scripts/fuzzy.js
@@ -1,21 +1,57 @@
+function fuzzyRawDate(date) {
+    if(date.getHours() < 4) {
+        return "Night"
+    } else if(date.getHours() < 11) {
+        return "Morning"
+    } else if(date.getHours() < 14) {
+        return "Noon"
+    } else if(date.getHours() < 17) {
+        return "Afternoon"
+    } else return "Night";
+}
+
+const oneDay = 1000 * 60 * 60 * 24;
+
+function getMidnight(day){
+    const date = new Date(day);
+    date.setMilliseconds(999);
+    date.setSeconds(59);
+    date.setMinutes(59);
+    date.setHours(23);
+    return date;
+}
+
+function isTomorrow(date){
+    const midnightTonight = getMidnight(new Date());
+    const midnightTomorrow = new Date(midnightTonight.getTime() + oneDay);
+
+    return date > midnightTonight && date < midnightTomorrow;
+}
+
 $(document).ready(() => {
     prettyPrintDiff = function (diff, long) {
         if(diff.days > 7) {
-            return "More than a week"
+            return "More Than a Week"
         } else if(diff.days > 1) {
-            return "One week"
+            return "One Week"
         } else if(diff.days === 1 || diff.hours > 12) {
-            return "One day"
+            return "One Day"
         } else if(diff.hours > 6) {
-            return "Several hours"
+            return "Several Hours"
         } else if(diff.hours > 1) {
-            return "A few hours"
+            return "A Few Hours"
         } else if(diff.hours === 1 || diff.minutes > 45) {
-            return "One hour"
+            return "An Hour"
         } else if(diff.minutes > 20) {
-            return "Half an hour"
-        } else if(diff.minutes > 5) {
-            return "A few minutes"
-        } else return "Soon"
+            return "Half an Hour"
+        } else return "A Few Minutes"
+    }
+
+    prettyDate = function (date) {
+        if(isTomorrow(date)) {
+            return `Tomorrow ${fuzzyRawDate(date)}`;
+        }
+
+        return fuzzyRawDate(date);
     }
 })
