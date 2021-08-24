d6f862fe5b8df752238229a3fbc238bb5a2b978b
dfsek <dfsek@protonmail.com>
2021-08-23 21:05:54 -0700

    add link to script page

--- resources/script/userscript.js
+++ resources/script/userscript.js
@@ -8,7 +8,7 @@ let scripts = window.localStorage.getItem("scripts");
 if(scripts && !iFramed) {
     let parsed = JSON.parse(scripts);
     for(let script of parsed) {
-        console.log(`Injecting script ${script.id}`);
+        console.log(`Injecting script "${script.id}"`);
         new Function(script.contents).apply(null);
         // eval(script.contents); // security is for losers
     }
