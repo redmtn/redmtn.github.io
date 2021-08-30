41b032f62e9ef6e7eee74c869d0efb1741ecf920
dfsek <dfsek@protonmail.com>
2021-08-30 14:44:49 -0700

    dont use year

--- resources/script/rule.js
+++ resources/script/rule.js
@@ -17,7 +17,7 @@ class Rule {
     }
 
     static #datesAreOnSameDay = (first, second) =>
-        first.getFullYear() === second.getFullYear() &&
+        // first.getFullYear() === second.getFullYear() &&
         first.getMonth() === second.getMonth() &&
         first.getDate() === second.getDate();
 
